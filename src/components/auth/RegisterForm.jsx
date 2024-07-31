e7fb571ec348957220b2997525ce7d2755c3e186
Yahir Alberto <136031489+yahirdev13@users.noreply.github.com>
2024-07-31 09:52:59 -0600

    [FIX] correcion de codigo y eliminacion de console logs

--- src/components/auth/RegisterForm.jsx
+++ src/components/auth/RegisterForm.jsx
@@ -5,11 +5,38 @@ import { doPost } from "../../config/Axios";
 export default function RegisterForm() {
 
   const login = async () => {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const response = await doPost('/auth/login', { email, password });
+
 
   }
 
   return (
     <form>
+      <div className="mb-3">
+        <label for="fullname">Nombre completo</label>
+        <input type="text" className="form-control" id="fullname" />
+      </div>
+      <div className="mb-3">
+        <label for="user">Nombre de usuario</label>
+        <input type="text" className="form-control" id="user" />
+      </div>
+      <div className="mb-3">
+        <label for="password">Contraseña</label>
+        <input type="password" className="form-control" id="password" />
+      </div>
+      <button type="submit" className="btn btn-primary" style={styles.buttton} onClick={
+        (e) => {
+          e.preventDefault();
+          login();
+        }
+      }>
+        Registrarse
+      </button>
+      <div className="text-center mt-2">
+        <a href="/login">Iniciar sesión</a>
+      </div>
 
     </form>
   );
